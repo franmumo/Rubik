8979711ea3e39ced615fde14be26d8bb984fa414
Edu Perez <edupvalera@gmail.com>
2014-05-20 19:10:12 +0200

    añadir constructor y enum

--- Rubik Cube/src/main/Rubik.java
+++ Rubik Cube/src/main/Rubik.java
@@ -1,7 +1,18 @@
 package main;
 
+
 public class Rubik {
+	private color cube[][][];
+
+	public enum color {
+		BLUE, YELLOW, GREEN, WHITE, ORANGE, RED
+	}
+
+	public Rubik() {
+		cube = new color[6][3][3];
+	}
+
 	public static void main(String[] args) {
-		
+		Rubik r = new Rubik();
 	}
 }
